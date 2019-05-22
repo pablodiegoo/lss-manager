91e86e2225bda46241956c57a962f2b2cf7b318d
Mihai Marica <mihai.marica82@yahoo.com>
2019-05-23 00:32:23 +0300

    created the first table (user)

---
+++ backend/src/Entity/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $displayName;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDisplayName(): ?string
+    {
+        return $this->displayName;
+    }
+
+    public function setDisplayName(string $displayName): self
+    {
+        $this->displayName = $displayName;
+        return $this;
+    }
+}
